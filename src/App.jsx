3fe6fb4cd028b721c5d4b95781c31206f3ef95b6
rajmanna <dev.rajm8923@gmail.com>
2025-08-14 16:39:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500">Hello World!</h1>
+    </main>
+  );
+};
+
+export default App;
