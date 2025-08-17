b74dd98cfa2f3f91bcf5630cb4dfee01074b1a81
rajmanna <dev.rajm8923@gmail.com>
2025-08-17 18:17:09 +0530

    Commented dropdown option

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -7,14 +7,14 @@ const Dropdown = () => {
         <span>No category</span>
         <ChevronDown size={18} strokeWidth={1} />
       </button>
-      <div className="select-element border border-slate-300 rounded-md mt-1.5">
+      {/* <div className="select-element border border-slate-300 rounded-md mt-1.5">
         <ul className="list-none m-0 p-1">
           <li className="px-3 py-1.5 rounded bg-gray-100 flex items-center space-x-2">
             <Check size={16} />
             <span>Today</span>
           </li>
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
