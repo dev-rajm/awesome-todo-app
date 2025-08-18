308edca802913de63fbdfb21399000777c0d63be
rajmanna <dev.rajm8923@gmail.com>
2025-08-18 11:17:59 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="min-h-dvh flex flex-col">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col justify-center items-center h-28 border-t border-t-slate-200">
+      <p className="text-slate-600 mb-2">
+        &copy; 2025 Awesome React Notes. All rights reserved.
+      </p>
+      <a>Sponsor Us</a>
+    </footer>
+  );
+};
+
+export default Footer;
