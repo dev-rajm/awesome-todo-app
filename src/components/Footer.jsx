36977a863e0c6f62f10e52bea29e2f0fb97606a3
rajmanna <dev.rajm8923@gmail.com>
2025-08-18 16:58:01 +0530

    Fixed footer height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center h-28 border-t border-t-slate-200 text-sm">
+    <footer className="flex flex-col justify-center items-center h-24 border-t border-t-slate-200 text-sm">
       <p className="text-slate-600 mb-2">
         &copy; 2025 Awesome React Notes. All rights reserved.
       </p>
