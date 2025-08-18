b18ce74155492648e66b587e414f34400fa46fe9
rajmanna <dev.rajm8923@gmail.com>
2025-08-18 11:37:03 +0530

    Fixed font size on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center h-28 border-t border-t-slate-200">
-      <p className="text-slate-600 mb-2 text-sm">
+    <footer className="flex flex-col justify-center items-center h-28 border-t border-t-slate-200 text-sm">
+      <p className="text-slate-600 mb-2">
         &copy; 2025 Awesome React Notes. All rights reserved.
       </p>
-      <a>Sponsor Us</a>
+      <a href="#">Sponsor Us</a>
     </footer>
   );
 };
