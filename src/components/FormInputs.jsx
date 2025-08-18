b20a31359136074b20c2721ed352a34444e4a191
rajmanna <dev.rajm8923@gmail.com>
2025-08-18 17:14:29 +0530

    Replace static textarea with dynamic textarea

--- src/components/FormInputs.jsx
+++ src/components/FormInputs.jsx
@@ -1,21 +1,35 @@
-import { CirclePlus } from "lucide-react";
-import Dropdown from "./Dropdown";
+import { CirclePlus } from 'lucide-react';
+import Dropdown from './Dropdown';
+import { useEffect, useRef, useState } from 'react';
 
 const FormInputs = () => {
+  const textAreaRef = useRef(null);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (!textAreaRef.current) return;
+    textAreaRef.current.style.height = 'auto';
+    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+  }, [value, textAreaRef]);
+
   return (
     <div className="form-element">
       <input
         type="text"
         name="title"
-        className="w-full p-2.5 rounded-md text-sm font-bold border border-slate-100 focus:outline-2 focus:outline-black focus:outline-offset-2"
+        className="w-full p-2.5 rounded-md text-sm font-semibold border border-slate-100 focus:outline-2 focus:outline-black focus:outline-offset-2"
         placeholder="Note title (optional)"
       />
       <div className="mb-6">
         <textarea
+          ref={textAreaRef}
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          rows={2}
           name="description"
           id="desc"
           placeholder="Start typing your note..."
-          className="h-auto min-h-16 shadow w-full rounded-md border border-slate-100 focus:outline-black focus:outline-2 focus:outline-offset-2 p-2.5 mt-3 font-medium resize-none placeholder:text-gray-400/85 focus:bg-slate-50"
+          className="shadow w-full rounded-md border border-slate-100 focus:outline-black focus:outline-2 focus:outline-offset-2 p-2.5 mt-3 font-medium resize-none overflow-y-hidden placeholder:text-gray-400/85 focus:bg-slate-50"
         ></textarea>
         <label
           htmlFor="desc"
