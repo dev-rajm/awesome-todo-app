8caefa0d4b02b8db6f7e142454b3cc1db1e2ded0
rajmanna <dev.rajm8923@gmail.com>
2025-08-19 13:24:24 +0530

    Make header not selectable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,17 @@
-import { FilePenLine } from "lucide-react";
+import { FilePenLine } from 'lucide-react';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center w-full mx-auto max-w-3xl flex-wrap gap-2 px-6 py-4">
+    <header className="flex justify-between items-center w-full mx-auto max-w-3xl flex-wrap gap-2 px-6 py-4 select-none">
       <div className="flex items-center gap-2">
         <FilePenLine size={20} strokeWidth={1} />
         <h1 className="font- tracking-tight">Awesome React Notes</h1>
       </div>
       <div>
         <p className="text-sm text-slate-600">
-          Sponsored by:{" "}
+          Sponsored by:{' '}
           <a href="#" className="text-blue-600">
-            {"(No One)"}
+            {'(No One)'}
           </a>
         </p>
       </div>
