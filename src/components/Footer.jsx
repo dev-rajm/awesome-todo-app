64359a0a58875300be27121a1a1ce2426b3ceaeb
rajmanna <dev.rajm8923@gmail.com>
2025-09-17 12:29:59 +0530

    updated footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col justify-center items-center h-24 border-t border-t-slate-200 text-sm">
       <p className="text-slate-600 mb-2">
-        &copy; 2025 Awesome React Notes. All rights reserved.
+        &copy; 2025 Awesome Notes. All rights reserved.
       </p>
       <a href="#">Sponsor Us</a>
     </footer>
