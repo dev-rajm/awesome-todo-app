629d6964c956e84d78577729a36ef093898529ab
rajmanna <dev.rajm8923@gmail.com>
2025-09-17 10:46:30 +0530

    implemented save notes logic

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,23 +1,52 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useNoteStore = create((set, get) => ({
-  title: '',
-  description: '',
-  wordCount: 0,
+const useNoteStore = create(
+  persist(
+    (set, get) => ({
+      title: '',
+      description: '',
+      wordCount: 0,
+      notes: [],
 
-  setTitle: title => set({ title }),
+      setTitle: title => set({ title }),
 
-  setDescription: description => {
-    const wordCount =
-      description.trim() === '' ? 0 : description.trim().split(/\s+/).length;
+      setDescription: description => {
+        const wordCount =
+          description.trim() === ''
+            ? 0
+            : description.trim().split(/\s+/).length;
 
-    set({ description, wordCount });
-  },
+        set({ description, wordCount });
+      },
 
-  isSaveDisable: () => {
-    const { title, description } = get();
-    return title.trim() === '' && description.trim() === '';
-  },
-}));
+      saveNote: () => {
+        const { title, description, notes } = get();
+        if (title.trim() && description.trim()) {
+          const newNote = {
+            id: Date().now(),
+            title,
+            description,
+          };
+
+          set({
+            notes: [...notes, newNote],
+            title: '',
+            description: '',
+            wordCount: 0,
+          });
+        }
+      },
+
+      isSaveDisable: () => {
+        const { title, description } = get();
+        return title.trim() === '' && description.trim() === '';
+      },
+    }),
+    {
+      name: 'notes-storage', // key in localstorage
+    }
+  )
+);
 
 export default useNoteStore;
