f5f779be351767ab6f4a45a89a252c908a910bea
rajmanna <dev.rajm8923@gmail.com>
2025-09-19 18:47:24 +0530

    added categorier to dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,19 +4,27 @@ import useNoteStore from '../store/useNoteStore';
 
 const Dropdown = () => {
   const [toggle, setToggle] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const { categories } = useNoteStore();
+
+  const handleSelect = cat => {
+    setSelectedCategory(cat);
+    setToggle(false);
+  };
 
   return (
     <div className="w-44 font-medium text-sm relative">
       <button
         onClick={() => setToggle(prev => !prev)}
-        className="flex justify-between items-center w-full border border-slate-200 px-3 py-2 rounded-md"
+        className="flex justify-between items-center w-full border border-slate-200 px-3 py-2 rounded-md focus:outline-2 outline-offset-3"
       >
-        <span>No category</span>
+        <span>
+          {selectedCategory ? selectedCategory.categoryName : 'No category'}
+        </span>
         <ChevronDown size={18} strokeWidth={1} />
       </button>
       <div
-        className={`select-element absolute top-10 w-full border border-slate-300 rounded-md mt-1.5 transform transition-all duration-500 ${
+        className={`select-element bg-white absolute top-10 w-full border border-slate-300 rounded-md mt-1.5 transform transition-all duration-500 ${
           toggle
             ? 'opacity-100 translate-y-0 visible'
             : 'opacity-0 -translate-y-4 invisible'
@@ -26,9 +34,13 @@ const Dropdown = () => {
           {categories.map(cat => (
             <li
               key={cat.id}
-              className="px-3 py-1.5 rounded bg-gray-100 flex items-center space-x-2"
+              onClick={() => handleSelect(cat)}
+              className={`px-3 py-1.5 rounded hover:bg-gray-50 flex items-center space-x-2 ${
+                selectedCategory?.id === cat.id && 'bg-gray-100'
+              }`}
             >
-              <Check size={16} />
+              {selectedCategory?.id === cat.id && <Check size={16} />}
+
               <span
                 className="w-4 h-4 rounded-full"
                 style={{ backgroundColor: `#${cat.selectedColor}` }}
