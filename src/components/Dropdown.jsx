abe295ec13b0f30be385cc764726574b4e121638
rajmanna <dev.rajm8923@gmail.com>
2025-10-05 21:18:06 +0530

    Updated dropdown component v1

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,20 +1,55 @@
 import { Check, ChevronDown } from 'lucide-react';
+import { useState } from 'react';
+
+import useNoteStore from '../store/useNoteStore';
 
 const Dropdown = () => {
+  const { categories, setSelectedCategoryId } = useNoteStore();
+  const [selectedValue, setSeletedValue] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(categories);
+
   return (
-    <div className="w-44 font-medium text-sm">
-      <button className="flex justify-between items-center w-full border border-slate-200 px-3 py-2 rounded-md">
-        <span>No category</span>
+    <div className="w-44 font-medium text-sm relative">
+      <div
+        className="flex justify-between items-center w-full border border-slate-200 px-3 py-2 rounded-md cursor-pointer focus:outline-2 outline-offset-2"
+        onClick={() => setIsOpen(prev => !prev)}
+      >
+        {selectedValue ? selectedValue : 'No category'}
         <ChevronDown size={18} strokeWidth={1} />
-      </button>
-      {/* <div className="select-element border border-slate-300 rounded-md mt-1.5">
-        <ul className="list-none m-0 p-1">
-          <li className="px-3 py-1.5 rounded bg-gray-100 flex items-center space-x-2">
-            <Check size={16} />
-            <span>Today</span>
+      </div>
+      {isOpen && (
+        <ul className="absolute space-y-1 bg-white w-full rounded-md px-2 py-1 transition duration-300 ease-in-out">
+          <li
+            className="hover:bg-slate-100 p-2 rounded cursor-pointer"
+            onClick={() => {
+              setSelectedCategoryId(null);
+              setSeletedValue(null);
+              setIsOpen(false);
+            }}
+          >
+            No category
           </li>
+          {categories.map(cat => (
+            <li
+              className="hover:bg-slate-100 p-2 rounded cursor-pointer flex items-center space-x-2"
+              key={cat.id}
+              onClick={() => {
+                setSelectedCategoryId(cat.id);
+                setSeletedValue(cat.categoryName);
+                setIsOpen(false);
+              }}
+            >
+              <span
+                className="w-4 h-4 rounded-full"
+                style={{ backgroundColor: `#${cat.selectedColor}` }}
+              ></span>
+              <span>{cat.categoryName}</span>
+            </li>
+          ))}
         </ul>
-      </div> */}
+      )}
     </div>
   );
 };
